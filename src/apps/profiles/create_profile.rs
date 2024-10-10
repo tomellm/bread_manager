11a1849fb90e079e24e00e6ca8266f1a1f77663e
tomellm <thomas.ellmenreich@gmail.com>
2024-10-10 22:58:57 +0200

    insert fix

--- src/apps/profiles/create_profile.rs
+++ src/apps/profiles/create_profile.rs
@@ -130,7 +130,7 @@ impl CreateProfile {
     fn save_profile(&mut self) -> Result<impl FnOnce() -> ImmediateValuePromise<ChangeResult>, ()> {
         self.update_builder();
         let profile = (*self.profile_builder).clone().build().map_err(|()| {})?;
-        let mut setter = self.profiles_communicator.update_action();
+        let mut setter = self.profiles_communicator.insert_action();
         Ok(move || ImmediateValuePromise::new(setter(profile)))
     }
 }
